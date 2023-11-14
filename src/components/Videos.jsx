d041a2ec72cf8773cd8940ec74640ed5ba171982
Omar Bazeed <omarbazeed@gmail.com>
2023-11-14 09:45:12 +0300

    Updates

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -17,12 +17,12 @@ if(!videos?.length) return (<p className='text-danger text-center'>Loading...</p
 
 return (
     <div className='text-light myVideosCom' style={{height,overflowY,overflowX,width,margin}}>
-        <div className='text-light'> 
+        <div className='text-light my-2'> 
             <span className='fw-bold fs-2'> {selectedCategory} </span>
             <span className='fw-bold fs-2 text-danger'> Videos </span>
         </div>
 
-        <div className='row justify-content-xs-center bg-dark'>
+        <div className='row justify-content-xs-center bg-dark py-3'>
             {videos.map((item,i)=>{
                 return(
                     <div key={i} className='col-xs-12 col ParticularSectionVideo mb-2'>
